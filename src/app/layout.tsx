9b8cb257e809a8004456045b0cf493b567b55812
atishaytuli07 <atishaytuliiaf@gmail.com>
2026-02-10 06:26:41 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono, Chewy } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -184,6 +185,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${chewy.variable} antialiased`}
       >
         {children}
+        <Analytics />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
